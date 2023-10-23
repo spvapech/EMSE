58c39d65fccb3c91af27296498a7783f3f8b28ab
shanenbe <stefan.hanenberg@uni-due.de>
2023-10-23 14:56:34 +0200

    Vorschlag Code

--- Uebung01/code.js
+++ Uebung01/code.js
@@ -9,7 +9,104 @@
 // WICHTIG: Man sollte new_random_integer nur innerhalb  der Lambda-Funktion ausführen, also NICHT
 // an einer anderen Stelle, damit man ein reproduzierbares Experiment erhält!
 
-let aString = "Hello World";
+function random_int() {
+    return document.new_random_integer(10);
+}
+
+function random_bool() {
+    return document.new_random_integer(2) > 0;
+}
+
+function create_statement(depth) {
+    if(depth==1) {
+        return new Return(random_int());
+    } else if (depth==2) {
+        let if_statement = new IfStatement();
+        if_statement.condition = random_bool();
+        if_statement.then_branch = new Return(random_int())
+        if_statement.else_branch = new Return(random_int());
+        return if_statement;
+
+    } else if (depth > 2) {
+        let if_statement = new IfStatement();
+        if_statement.condition = random_bool();
+        if_statement.then_branch = create_statement(depth-1);
+        if_statement.else_branch = create_statement(depth-1)        ;
+        return if_statement;
+    }
+
+    throw "Sollte nicht sein";
+}
+
+
+class Statement {
+    indented_code(indentation_level) {}
+    non_indented_code() {}
+
+    return_value() {}
+}
+
+class Return extends Statement {
+    value;
+    constructor(value) {
+        super();
+        this.value=value;
+    }
+
+    indented_code(indentation_level) {
+        let ret = "    ".repeat(indentation_level);
+        ret = ret + "return " + this.value + "\n";
+        return ret;
+    }
+
+    non_indented_code() {
+        let ret = "return " + this.value + "\n";
+        return ret;
+    }
+
+    return_value() {
+        return this.value;
+    }
+}
+class IfStatement extends Statement {
+    condition;
+    then_branch;
+    else_branch;
+
+    indented_code(indentation_level) {
+        let ret = "    ".repeat(indentation_level);
+        ret = ret + "if (" + this.condition + ") {\n";
+        indentation_level = indentation_level + 1;
+        ret = ret + this.then_branch.indented_code(indentation_level);
+        indentation_level = indentation_level - 1;
+        ret = ret + "    ".repeat(indentation_level) + "} else {\n"
+        indentation_level = indentation_level + 1;
+        ret = ret + this.else_branch.indented_code(indentation_level);
+        indentation_level = indentation_level - 1;
+        ret = ret + "    ".repeat(indentation_level) + "}\n"
+        return ret;
+    }
+
+    non_indented_code() {
+        let ret = "";
+        ret = ret + "if (" + this.condition + ") {\n";
+        ret = ret + this.then_branch.non_indented_code();
+        ret = ret + "} else {\n"
+        ret = ret + this.else_branch.non_indented_code();
+        ret = ret + "}\n"
+        return ret;
+    }
+
+    return_value() {
+        if(this.condition==true)
+            return this.then_branch.return_value();
+        return this.else_branch.return_value();
+    }
+
+}
+
+let stmt = create_statement(3);
+console.log(stmt.non_indented_code(0));
 
 // Das hier ist die eigentliche Experimentdefinition
 document.experiment_definition(
@@ -35,12 +132,14 @@ document.experiment_definition(
             //     variable - Ein Variable-Objekt, welches das Feld name hat (der Name der Variablen);
             //     value - Ein String, in dem der Wert des Treatments steht.
 
-            if (t.treatment_combination[0].value=="indented") // fragt, ob die erste Variable (die einzige) den Wert "indented" hat
-                t.code = "    " + aString;
+            let stmt = create_statement(4);
+            if (t.treatment_combination[0].value=="indented")
+                t.code = stmt.indented_code(0);
             else
-                t.code = aString;
+                t.code = stmt.non_indented_code();
 
-            t.expected_answer = "1";
+            t.expected_answer = "" + stmt.return_value();
+
 
             // im Feld after_task_string steht eine Lambda-Funktion, die ausgeführt wird
             // wenn eine Task beantwortet wurde. Das Ergebnis der Funktion muss ein String
